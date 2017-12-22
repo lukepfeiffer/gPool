ff9a9eb7a35e3ff1cb8f16e0ae17ba8aec2f1682
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-12-22 14:16:06 -0500

    Style homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeNavComponent } from './home-nav/home-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    HomeNavComponent
+    HomeNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
